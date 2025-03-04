6d90413e9a2339f898c0baa98cc6b2ee119724e0
Arvind Akula <ArvindKumar.Akula@gmail.com>
2025-03-04 11:41:36 -0800

    Add TodoService class

---
+++ backend/src/main/java/com/example/service/TodoService.java
@@ -0,0 +1,44 @@
+package com.example.service;
+
+import com.example.entity.Todo;
+import com.example.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Optional<Todo> getTodoById(Long id) {
+        return todoRepository.findById(id);
+    }
+
+    public Todo createTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public Todo updateTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public boolean deleteTodo(Long id) {
+        if (todoRepository.existsById(id)) {
+            todoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
